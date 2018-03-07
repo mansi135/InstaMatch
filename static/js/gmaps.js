de335cbf8afb85bc62beb4ee86655c36935931c1
Mansi Agarwal <mansi.agarwal1410@gmail.com>
2018-03-07 04:51:40 +0000

    styling changes

--- static/js/gmaps.js
+++ static/js/gmaps.js
@@ -7,7 +7,7 @@
   function initMap() {
 
       map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 6,
+        zoom: 7,
        // center: mylocation
       });
       geocoder = new google.maps.Geocoder();
